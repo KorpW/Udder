1be11ead34633d1297f18ecb9ebaaca43715af12
Magnus Wærstad <magnus@magnuswaerstad-MacBookPro.local>
2018-02-11 22:21:35 +0100

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Magnus Eri Wærstad on 11.02.2018.
+//  Copyright © 2018 Magnus Eri Wærstad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
